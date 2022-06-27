44fb5a8b9f42b39aec15a18fc16853ada99a1410
rakibmalik <rakib@webcluesinfotech.com>
2022-06-27 19:15:29 +0530

    Changed structure

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 // styles from '../styles/Home.module.css'
-import Forminput from './components/Forminput'
-import List from './components/List'
-import Footer from './components/Footer'
-import {DataProvider} from './components/DataProvider'
+import Forminput from './Forminput'
+import List from './List'
+import Footer from './Footer'
+import {DataProvider} from './DataProvider'
 
 export default function Home() {
   return (
